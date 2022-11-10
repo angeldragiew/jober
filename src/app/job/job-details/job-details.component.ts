4590f7eec0ed7c232b17a3f8b557bc4cb50e6e42
Angel Dragiev <angeldragieww@gmail.com>
2022-11-10 12:07:21 +0200

    Refactored ngOnIt function to work with switchMap

--- src/app/job/job-details/job-details.component.ts
+++ src/app/job/job-details/job-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { JobService } from 'src/app/services/job.service';
 import IJob from 'src/app/models/job.model';
+import { AuthService } from 'src/app/services/auth.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-job-details',
@@ -13,14 +15,19 @@ export class JobDetailsComponent implements OnInit {
   activeJob: IJob | null = null
 
   constructor(private route: ActivatedRoute,
-    private jobService: JobService) { }
+    private jobService: JobService,
+    public auth: AuthService) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.jobId = params['id']
-    });
-
-    this.jobService.getJob(this.jobId).subscribe(job => this.activeJob = job)
+    this.route.params.pipe(
+      switchMap(params => {
+        return this.jobService.getJob(params['id'])
+      })
+    ).subscribe(jobData => {
+      this.activeJob = jobData;
+      console.log(this.activeJob)
+      this.jobId = jobData.docId ?? ''
+    })
   }
 
 
