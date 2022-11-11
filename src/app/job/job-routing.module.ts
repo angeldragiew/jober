b91beb47eec56bd7daccea20ecd97f7bbd961104
Angel Dragiev <angeldragieww@gmail.com>
2022-11-11 18:13:30 +0200

    Added routes and links

--- src/app/job/job-routing.module.ts
+++ src/app/job/job-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppliedJobsComponent } from './applied-jobs/applied-jobs.component';
 import { BrowseJobsComponent } from './browse-jobs/browse-jobs.component';
 import { CreateJobComponent } from './create-job/create-job.component';
+import { CreatedJobsComponent } from './created-jobs/created-jobs.component';
 import { EditJobComponent } from './edit-job/edit-job.component';
 import { JobDetailsComponent } from './job-details/job-details.component';
 import { JobComponent } from './job/job.component';
@@ -23,6 +25,14 @@ const routes: Routes = [
     path: 'EditJob/:id',
     component: EditJobComponent
   },
+  {
+    path: 'MyCreatedJobs',
+    component: CreatedJobsComponent
+  },
+  {
+    path:'MyAppliedJobs',
+    component:AppliedJobsComponent
+  }
 ];
 
 @NgModule({
