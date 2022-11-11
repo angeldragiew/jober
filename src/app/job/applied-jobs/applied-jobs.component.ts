13e24036c8d8ddb03e7883f3dc93d10a9c16372f
Angel Dragiev <angeldragieww@gmail.com>
2022-11-11 18:12:05 +0200

    Added component so user can see the jobs he applied for

---
+++ src/app/job/applied-jobs/applied-jobs.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { JobService } from 'src/app/services/job.service';
+import { AuthService } from 'src/app/services/auth.service';
+import IJob from 'src/app/models/job.model';
+import { switchMap, map, EMPTY } from 'rxjs';
+import ICandidate from 'src/app/models/candidate.model';
+
+@Component({
+  selector: 'app-applied-jobs',
+  templateUrl: './applied-jobs.component.html',
+  styleUrls: ['./applied-jobs.component.css']
+})
+export class AppliedJobsComponent implements OnInit {
+  jobs: IJob[] = []
+
+  constructor(private jobService: JobService,
+    private auth: AuthService) { }
+
+  ngOnInit(): void {
+    this.jobs = []
+
+    this.auth.user$.pipe(
+      switchMap(user => {
+        if (!user) {
+          return EMPTY
+        }
+        const userId = user.uid
+        return this.jobService.getAllJobs().pipe(map(jobs => ({ jobs, userId })))
+      })
+    ).subscribe(({ jobs, userId }) => {
+      this.jobs = jobs.filter(j => (j.candidates as Array<ICandidate>).filter(c => c.uid == userId).length > 0).map(j=> {
+        return {
+          ...j,
+          status: (j.candidates as Array<ICandidate>).find(c=>c.uid==userId)?.status
+        }
+      })
+    })
+  }
+}
